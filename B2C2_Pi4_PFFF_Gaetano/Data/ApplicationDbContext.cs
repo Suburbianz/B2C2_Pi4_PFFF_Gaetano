ad20f60ca889e2d44d856f8129690bc0e39a34bc
Gaétano Waltje <2101492waltje@zuyd.nl>
2022-09-28 09:18:29 +0200

    Add project files.

---
+++ B2C2_Pi4_PFFF_Gaetano/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace B2C2_Pi4_PFFF_Gaetano.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ B2C2_Pi4_PFFF_Gaetano/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace B2C2_Pi4_PFFF_Gaetano.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
