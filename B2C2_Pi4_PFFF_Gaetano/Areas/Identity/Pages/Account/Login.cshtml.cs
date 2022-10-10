b4892cf33311639e1fb71af5faa17f20258ebd29
DESKTOP-K11RPBP\Gaetano <gaetanodubois9@gmail.com>
2022-10-10 08:33:48 +0200

    Translation login

--- B2C2_Pi4_PFFF_Gaetano/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ B2C2_Pi4_PFFF_Gaetano/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -65,6 +65,7 @@ namespace B2C2_Pi4_PFFF_Gaetano.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
+            [Display(Name = "Gebruikersnaam")]
             [Required]
             public string UserName { get; set; }
 
@@ -72,6 +73,7 @@ namespace B2C2_Pi4_PFFF_Gaetano.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
+            [Display(Name = "Wachtwoord")]
             [Required]
             [DataType(DataType.Password)]
             public string Password { get; set; }
